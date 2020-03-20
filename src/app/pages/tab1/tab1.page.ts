6ddcad8e8618623f710bcddaa458bf42689b9783
Edwin Alejandro Tello Santizo <alextello1@hotmail.com>
2020-03-20 13:07:05 -0600

    navegación a nueva página add

--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { DeseosService } from '../../services/deseos.service';
+import { Lista } from '../../models/lista.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tab1',
@@ -7,7 +9,13 @@ import { DeseosService } from '../../services/deseos.service';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  listas: Lista[];
+  constructor(public deseosService: DeseosService,
+              private router: Router) {
 
-  constructor(public deseosService: DeseosService) {}
+  }
 
+  agregarLista() {
+    this.router.navigateByUrl('/tabs/tab1/agregar');
+  }
 }
