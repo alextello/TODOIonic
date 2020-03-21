81d2469a81d101b393899cee674896433dab7e1a
Edwin Alejandro Tello Santizo <alextello1@hotmail.com>
2020-03-20 18:44:39 -0600

    Añadida funcionalidad de localStorage

--- src/app/services/deseos.service.ts
+++ src/app/services/deseos.service.ts
@@ -8,14 +8,27 @@ export class DeseosService {
   listas: Lista[] = [];
 
   constructor() {
-      const lista1 = new Lista('Recolectar gemas del inifinito');
-      const lista2 = new Lista('Héroes a desaparecer');
-      this.listas.push(lista1, lista2);
-
+    this.cargarStorage();
+    // const lista1 = new Lista('Recolectar gemas del inifinito');
+    // const lista2 = new Lista('Héroes a desaparecer');
+    // this.listas.push(lista1, lista2);
    }
 
    crearLista(titulo: string) {
     const nuevaLista = new Lista(titulo);
     this.listas.push(nuevaLista);
+    this.guardarStorage();
+   }
+
+   guardarStorage() {
+      localStorage.setItem('data', JSON.stringify(this.listas));
+   }
+
+   cargarStorage() {
+    if (localStorage.getItem('data')) {
+      this.listas = JSON.parse(localStorage.getItem('data'));
+    } else {
+      this.listas = [];
+    }
    }
 }
